36a641dd3b50588f2c4c1223e1f9065994b0d1bd
gnubyte <phastings@openmobo.com>
2022-11-22 04:14:52 -0800

    Update setup.sh
    removed some useless lines, added a few more helpful ones.
    added nodejs, npm, ca-certificates

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
-uname -a >> systeminfo
-cat /etc/*release* >> systeminfo
-
 apt-get install sudo -y
 sudo apt-get update -y
 sudo apt-get install software-properties-common -y
@@ -10,6 +7,7 @@ sudo apt-get install ufw -y
 sudo apt-get install fail2ban -y
 sudo apt-get install python3 -y 
 sudo apt-get install python3-pip -y
+sudo apt-get install nodejs npm -y
 sudo apt-get install git -y
 sudo apt-get install default-jdk -y
 sudo apt-get install default-jre -y
@@ -17,6 +15,7 @@ sudo apt-get install build-essential -y
 sudo apt-get install manpages-dev -y
 sudo apt-get install htop -y 
 sudo apt install libssl-dev -y
+sudo apt install ca-certificates -y 
 
 
 sudo apt-get install podman
