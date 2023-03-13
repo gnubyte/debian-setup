aa0c64ce01f0e9a77b93a817754d34165edbacff
gnubyte <phastings@openmobo.com>
2023-03-13 11:22:02 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ sudo apt-get install manpages-dev -y
 sudo apt-get install htop -y 
 sudo apt install libssl-dev -y
 sudo apt install ca-certificates -y 
+sudo apt install snapd -y
 curl https://bun.sh/install | bash
 
 #podman
